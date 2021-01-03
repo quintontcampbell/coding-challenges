a5c231077674d77f3600b9384504f8936f38de72
Quinton Campbell <quinton.t.campbell@gmail.com>
2021-01-02 21:50:49 -0500

    refactor solution to coding challenge. return odd or even outlier

--- findOutlier.js
+++ findOutlier.js
@@ -1,12 +1,19 @@
+// function findOutlier(integers) {
+//   const evens = integers.filter(x => x % 2 === 0)
+//   const odds = integers.filter(x => x % 2 !== 0)
+//   if (evens.length > odds.length){
+//     return evens
+//   } 
+//   else {
+//     return odds
+//   }
+// }
 function findOutlier(integers) {
-// i actually did it backwards, you're supposed to return the outlier
   const evens = integers.filter(x => x % 2 === 0)
   const odds = integers.filter(x => x % 2 !== 0)
-  if (evens.length > odds.length){
-    return evens
-  } 
-  else {
-    return odds
+  if (evens.length === 1) {
+    return evens[0]
   }
+  else return odds[0]
 }
 export default findOutlier
--- findOutlier.test.js
+++ findOutlier.test.js
@@ -1,13 +1,23 @@
 import assert from 'assert'
 import findOutlier from './findOutlier.js'
 
-describe('Given an array of integers that is compromised of either only even or odd numbers, save for one number, return an array without that single number', () => {
-    it('removes odds', () => {
+// describe('Given an array of integers that is compromised of either only even or odd numbers, save for one number, return an array without that single number', () => {
+//     it('removes odds', () => {
+//       const result = findOutlier([0,1,2] )
+//       assert.deepStrictEqual(result, [0,2] )
+//     })
+//     it('removes evens', () => {
+//       const result = findOutlier([1,2,3])
+//       assert.deepStrictEqual(result, [1,3])
+//     })
+// })
+describe('Given an array of integers that is compromised of either only even or odd numbers, save for one number, return that number', () => {
+    it('returns 1', () => {
       const result = findOutlier([0,1,2] )
-      assert.deepStrictEqual(result, [0,2] )
+      assert.deepStrictEqual(result, 1 )
     })
-    it('removes evens', () => {
+    it('returns 2', () => {
       const result = findOutlier([1,2,3])
-      assert.deepStrictEqual(result, [1,3])
+      assert.deepStrictEqual(result, 2)
     })
 })
