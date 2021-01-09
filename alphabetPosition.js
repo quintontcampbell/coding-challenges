d3e1300e5afc50639b0f5fdc828a567bce13681b
Quinton Campbell <quinton.t.campbell@gmail.com>
2021-01-08 21:26:08 -0500

    Codewars kata maniupalting strings + Mocha Test

---
+++ alphabetPosition.js
@@ -0,0 +1,5 @@
+const alphabetPosition = () => {
+
+}
+
+export default alphabetPosition
---
+++ alphabetPosition.test.js
@@ -0,0 +1,10 @@
+import alphabetPosition from './alphabetPosition.js'
+import assert from 'assert'
+
+
+describe("In this kata you are required to, given a string, replace every letter with its position in the alphabet. If anything in the text isn't a letter, ignore it and don't return it.", () => {
+  it('returns the numbers', () => {
+    const result = alphabetPosition("The sunset sets at twelve o' clock.")
+    assert.deepStrictEqual(result, "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11")
+  })
+})
