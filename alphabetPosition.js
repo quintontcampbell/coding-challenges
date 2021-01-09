30baa73462ad7d9173f6f41e8e62bdc95f59ac16
Quinton Campbell <quinton.t.campbell@gmail.com>
2021-01-08 22:00:59 -0500

    The current gist of what I am trying out in alhaphabetPosition

--- alphabetPosition.js
+++ alphabetPosition.js
@@ -1,5 +1,14 @@
-const alphabetPosition = () => {
-
+const alphabetPosition = (text) => {
+  const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+  const textArray = text.split('')
+  let positions = ""
+  for (let i = 0; i < textArray.length; i++) {
+    positions += alphabet.filter(letter => {
+      letter.toLowerCase() === textArray[i]
+      return alphabet.indexOf(letter)
+    })
+  }
+  return positions
 }
 
 export default alphabetPosition
--- alphabetPosition.test.js
+++ alphabetPosition.test.js
@@ -7,4 +7,8 @@ describe("In this kata you are required to, given a string, replace every letter
     const result = alphabetPosition("The sunset sets at twelve o' clock.")
     assert.deepStrictEqual(result, "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11")
   })
+  it('returns different numbers', () => {
+    const result = alphabetPosition("The narwhal bacons at midnight.")
+    assert.deepStrictEqual(result, "20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20")
+  })
 })
